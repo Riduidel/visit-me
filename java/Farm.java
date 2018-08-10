6d2cf8cd0fa5db7a51fa447c9769ef4ebeb6726c
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2018-08-10 13:29:43 +0200

    java: étape 2 De façon classique, on crée une interface, mais ça pose quelques problèmes. D'abord, il faut définir méthode commune.(donc avec une signature commune). Ensuite, pour chaque méthode à laquelle on veut accéder, il faut ajouter une déclaration dans l'interface. Ca devient vite verbeux.

--- java/Farm.java
+++ java/Farm.java
@@ -1,13 +1,26 @@
-class A {
-	public void do_this() {
-		// TODO Auto-generated method stub
-
-	}
+interface Interface {
+	void do_it();
 }
-class B{
-	public void do_that() {
-		// TODO Auto-generated method stub
+class A implements Interface{
+	public void do_this() {
+		System.out.println("A do this");
+	}
 
+	@Override
+	public void do_it() {
+		do_this();
+	}
+	
+	
+}
+class B implements Interface{
+	public void do_that() {
+		System.out.println("B do that");
+	}
+	
+	@Override
+	public void do_it() {
+		do_that();
 	}
 }
 
@@ -17,8 +30,8 @@ public class Farm {
 		A a = new A();
 		B b = new B();
 
-		a.do_this();
-		b.do_that();
+		Interface truc = a;
+		truc.do_it();
 	}
 
 }
