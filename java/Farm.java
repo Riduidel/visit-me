5e70f5846801e9e9f0e53320b921217545306ec1
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2018-08-10 13:25:16 +0200

    java: étape 1 Que faire quand on a deux classes métier qu'on veut utiliser de la même manière et qui présentent des méthodes différentes ?

---
+++ java/Farm.java
@@ -0,0 +1,24 @@
+class A {
+	public void do_this() {
+		// TODO Auto-generated method stub
+
+	}
+}
+class B{
+	public void do_that() {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+public class Farm {
+
+	public static void main(String[] args) {
+		A a = new A();
+		B b = new B();
+
+		a.do_this();
+		b.do_that();
+	}
+
+}
